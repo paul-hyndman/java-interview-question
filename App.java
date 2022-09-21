68ea6625a6a1eeaa0638cb49adfa0a0ab2631655
paul-hyndman <110925830+paul-hyndman@users.noreply.github.com>
2022-09-21 09:49:22 -0500

    Delete App.java

--- App.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class App {
-    public static void main(String[] names) {
-        NameFinder nameFinder = new NameFinder(names);
-        System.out.println("Most popular name is " + nameFinder.getMostPopular());
-        System.out.println("Least popular name is " + nameFinder.getLeastPopular());
-    }
-}
